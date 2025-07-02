a209635fa52e86d19693af41a16a76cb3f4fc80e
Débora_Ferreira <deboracris29a@gmail.com>
2025-07-02 15:45:27 -0300

    Initial Commit

---
+++ Cosmos/CosmosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmosApp.swift
+//  Cosmos
+//
+//  Created by Débora Cristina Silva Ferreira on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct CosmosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
